5796519086af4e98c110b233e8a3d6a8e180a4f1
root <root@Alvien>
2024-08-30 17:45:28 +1200

    Added new functionality that enables the IDS agents to listen on the network traffic

--- src/include/errorHand.h
+++ src/include/errorHand.h
@@ -1,8 +1,7 @@
 #ifndef ERRORHAND
 #define ERRORHAND
 
-static int checkError(int output, char msg[]);
-
-static void* checkNull(void *output, char msg[]);
+int checkError(int output, char msg[]);
+void* checkNull(void *output, char msg[]);
 
 #endif
